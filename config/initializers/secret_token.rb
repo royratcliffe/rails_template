e6422e51c3ee03951efa47670be5436df3a20776
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2012-09-17 19:58:56 +0100

    Rails template application
    Rails 3.2.8, just for testing.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTemplate::Application.config.secret_token = 'fbcb442c2e8875f9d35d7dc53aac28d51a71df939898111ea983d6a80a6c8ed82a7ecebf1954cf8b56a8434d0c9c7f0046d82e0e005c11d6844c11c14065841a'
